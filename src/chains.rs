ce21dc8b39815b48fe78756896356dba0cb6dced
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-12-02 09:31:18 -0500

    add more chain names

--- src/chains.rs
+++ src/chains.rs
@@ -1,4 +1,10 @@
-pub const CHAINS: &[(u32, &str)] = &[(1, "Ethereum"), (8453, "Base"), (42161, "Arbitrum")];
+pub const CHAINS: &[(u32, &str)] = &[
+    (1, "Ethereum"),
+    (8453, "Base"),
+    (42161, "Arbitrum"),
+    (137, "Polygon"),
+    (10, "Optimism"),
+];
 
 pub fn get_chain_name(chain: u32) -> String {
     return CHAINS
